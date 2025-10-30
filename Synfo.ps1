cedbc910933c24e721261864e72889f4eb4b072e
Anjula M. <133040646+anjulameegalla@users.noreply.github.com>
2025-10-30 08:13:08 +0530

    Create Synfo.ps1

---
+++ Synfo.ps1
@@ -0,0 +1,4 @@
+# Synfo.ps1 - Enhanced System Diagnostic Script for Security and Reliability Checks
+# Optimized for UI clarity, hardware/security logging, and a focused display.
+
+#Under development!!!
